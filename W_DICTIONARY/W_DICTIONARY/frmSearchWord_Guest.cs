7feede8aef672e190fe64cb2fe089b90665d9299
tranxuanthin <63130987+tranxuanthin@users.noreply.github.com>
2020-11-07 06:39:19 +0700

    13/10
    13/10

---
+++ W_DICTIONARY/W_DICTIONARY/frmSearchWord_Guest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpeechLib;
+
+
+namespace W_DICTIONARY
+{
+    public partial class frmSearchWord_Guest : Form
+    {
+        public frmSearchWord_Guest()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=Dictionary;Integrated Security=True");  //connect sql
+
+          
+
+        private void frmSearchWord_Guest_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            Loaddata();
+        }
+
+        private void Loaddata()
+        {
+            
+            string sql = "SELECT * FROM ListWord";            
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            LboxItem.DataSource = dt;
+            LboxItem.DisplayMember = "English";   //display data on listbox
+            LboxItem.ValueMember = "idword";            //get id 
+            
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sqlSEARCH = "SELECT *FROM ListWord Where English=@english";
+                SqlCommand cmd = new SqlCommand(sqlSEARCH, con);
+                cmd.Parameters.AddWithValue("English", txtEnglish.Text);
+                cmd.ExecuteNonQuery();
+                SqlDataReader dr = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                if (dt.Rows.Count > 0)
+                {
+                    txtVNShort.Text = dt.Rows[0]["VNShort"].ToString();
+                    txtVNLong.Text = dt.Rows[0]["VNLong"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy từ của bạn vừa nhập");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+
+        private void LboxItem_SelectedIndexChanged(object sender, EventArgs e)   //When you click word on ListBox, another Textbox will show corresponding data to the word. 
+        {
+            try
+            {
+                string sqlSEARCH = "SELECT *FROM ListWord Where English=@english";
+                SqlCommand cmd = new SqlCommand(sqlSEARCH, con);
+                cmd.Parameters.AddWithValue("English", LboxItem.Text);
+                cmd.ExecuteNonQuery();
+                SqlDataReader dr = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                txtEnglish.Text = LboxItem.Text;
+                if (dt.Rows.Count > 0)
+                {
+                    txtID.Text = dt.Rows[0]["idword"].ToString();
+                    txtVNShort.Text = dt.Rows[0]["VNShort"].ToString();
+                    txtVNLong.Text = dt.Rows[0]["VNLong"].ToString();
+                    pb_Guest.Image = Image.FromFile(dt.Rows[0]["ImgLocation"].ToString());
+                    
+
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnListen_Click(object sender, EventArgs e)  //Voice
+        {
+            SpVoice phatam = new SpVoice();
+            phatam.Speak(txtEnglish.Text, SpeechVoiceSpeakFlags.SVSFDefault);
+        }
+
+
+    }
+}
